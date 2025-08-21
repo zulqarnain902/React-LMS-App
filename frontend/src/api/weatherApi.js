8ff1326d43c7ddf3f31cd358f9758ce25eae94bf
Zulqarnain khan <70657074+zulqarnain902@users.noreply.github.com>
2025-08-21 15:38:11 +0500

    weather api fixed

--- frontend/src/api/weatherApi.js
+++ frontend/src/api/weatherApi.js
@@ -1,7 +1,7 @@
 import axios from "axios"
 
 const api = axios.create({
-    baseURL: "http://goweather.xyz",
+    baseURL: "https://goweather.xyz/",
     headers: {
         "Content-Type": "application/json",
     },

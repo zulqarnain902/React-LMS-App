e3dda5a17a2a656fd29bfbcacbbc8eb1791e5228
Zulqarnain khan <70657074+zulqarnain902@users.noreply.github.com>
2025-08-21 15:28:05 +0500

    Cors fixed!

--- backend/app.js
+++ backend/app.js
@@ -17,7 +17,8 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 const corsOptions = {
-    origin: ["http://localhost:5173"],
+    // origin: ["http://localhost:5173"],
+    origin: ["https://react-lms-app.vercel.app"],
     optionsSuccessStatus: 200,
     credentials: true,
 };
